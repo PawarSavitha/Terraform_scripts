a5d90d702bae0e0f76c98c6f64545a09e6737f45
PawarSavitha <savithapawar038@gmail.com>
2023-09-19 15:17:35 +0530

    adding terraform scripts

---
+++ ec2linux-instance.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+            region = "us-west-1"
+            access-key = "AKIASDBK6MQWLRPIIGOG"
+            secrete_key = "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"
+        }
+    }
+}
+
+#resource block
+resource "aws_instance" "ubuntu"{
+    ami = "ami-0f8e81a3da6e2510a"
+    instance_type = "t2.micro"
+
+    tags = {
+        name = "HelloTerraform"
+    }
+}
